ab2f8ae06c29e1ecc537f99133115fe9e398f49a
Kevin Gross <kevin.gross@globant.com>
2019-05-23 14:18:08 -0300

    Fix name

--- v2/main_test.go
+++ v2/main_test.go
@@ -7,7 +7,7 @@ import (
     "github.com/gorilla/mux"
 )
 
-func TestIndex(t *testing.T) {
+func TestMain(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
     response := executeRequest(req)
     checkResponseCode(t, http.StatusOK, response.Code)
