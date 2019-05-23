d4869a7eb4955bf59fa2f282ffa00ebc730db6ac
Kevin Gross <kevin.gross@globant.com>
2019-05-23 14:15:33 -0300

    correct route

--- v2/main_test.go
+++ v2/main_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestGetEntries(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/users", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
     response := executeRequest(req)
     checkResponseCode(t, http.StatusOK, response.Code)
     if body := response.Body.String(); body != "Hello, / | Production Branch" {
